5932a97c97b9d534a85d95cb5aafb6952bf92be0
Sushil97 <sushil500530@gmail.com>
2023-10-11 20:14:50 +0600

    add to add user user to form section

--- src/MainLayout/Root.jsx
+++ src/MainLayout/Root.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
 
 const Root = () => {
+
+
+  const handleAddUser = e => {
+    e.preventDefault();
+    const form = e.target
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const user = {email,password}
+    console.log(user);
+
+    fetch('http://localhost:5000/users', {
+      method:"POST",
+      headers:{
+        'content-type': "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
   return (
     <div>
       <h1 className='text-3xl text-center my-5 font-bold'>Simple CRUD</h1>
+
+      <div className="hero h-[560px]  bg-base-200">
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleAddUser} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Add User</button>
+              </div>
+            </form>
+          </div>
+      </div>
     </div>
   );
 };
